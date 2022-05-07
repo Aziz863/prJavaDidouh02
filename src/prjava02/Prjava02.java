b2843a427ec57d8b069d6d286102a727436863f7
Aziz Didouh <aziz.r8@hotmail.com>
2022-05-07 20:52:14 +0200

    Treballant amb ordres Git i branques: Versió 1.3 del projecte prjavaDidouh02

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -19,6 +19,8 @@ public class Prjava02 {
     public static void main(String[] args) {
         System.out.println("Creació d'una branca del projecte prjavaDidouh02");
         System.out.println("Afegint més codi a la branca00 del projecte prjavaDidouh02");
+        System.out.println("Afegint codi a la branca01 del projecte prjavaDidouh02");
+
         try {
             InetAddress adreça = InetAddress.getLocalHost();
             String hostname = adreça.getHostName();
